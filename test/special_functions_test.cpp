1ab1374a759cafa42058950ef7cc979cd02464ac
Allen Goodman <goodmaa3@gene.com>
2023-03-27 13:20:21 -0400

    test

--- test/special_functions_test.cpp
+++ test/special_functions_test.cpp
@@ -1,31 +1,5 @@
 #include <gtest/gtest.h>
 
-#include "special_functions.h"
-
-TEST(cos_pi_test, accuracy) {
-    EXPECT_EQ(special_functions::cos_pi(1.0), -1.0);
-}
-
-TEST(cosh_pi_test, accuracy) {
-    EXPECT_DOUBLE_EQ(special_functions::cosh_pi(1.0), 11.591953275521519);
-}
-
-TEST(factorial_test, accuracy) {
-    EXPECT_EQ(special_functions::factorial<double>(1), 1.0);
-}
-
-TEST(sin_pi_test, accuracy) {
-    EXPECT_EQ(special_functions::sin_pi(1.0), -0.0);
-}
-
-TEST(sinh_pi_test, accuracy) {
-    EXPECT_DOUBLE_EQ(special_functions::sinh_pi(1.0), 11.548739357257746);
-}
-
-TEST(tan_pi_test, accuracy) {
-    EXPECT_EQ(special_functions::tan_pi(1.0), -0.0);
-}
-
-TEST(tanh_pi_test, accuracy) {
-    EXPECT_DOUBLE_EQ(special_functions::tanh_pi(1.0), 0.99627207622074998);
+TEST(foo_test, accuracy) {
+    EXPECT_EQ(0.0, 0.0);
 }
