b0826c16ea5da16d8cea70f9631c3ee2ae4b3861
Allen Goodman <goodmaa3@gene.com>
2023-03-28 18:11:22 -0400

    smoke tests

--- include/special_functions/zernike_polynomial_z.h
+++ include/special_functions/zernike_polynomial_z.h
@@ -7,9 +7,9 @@ namespace special_functions {
     template<typename T, typename U>
     inline special_functions::fp_promote_t<T, U>
     zernike_polynomial_z(unsigned int n, int m, T r, U p) {
-        using type = special_functions::fp_promote_t<T, U>;
+        using V = special_functions::fp_promote_t<T, U>;
 
-        return special_functions::detail::zernike_polynomial_z<type>(n, m, r, p);
+        return special_functions::detail::zernike_polynomial_z<V>(n, m, r, p);
     }
 }
 
