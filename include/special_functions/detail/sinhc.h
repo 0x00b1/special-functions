385965735cbedc3bfade48b3ed0efb1277c99ddc
Allen Goodman <goodmaa3@gene.com>
2023-03-28 16:24:51 -0400

    smoke tests

--- include/special_functions/detail/sinhc.h
+++ include/special_functions/detail/sinhc.h
@@ -2,16 +2,18 @@
 #define SPECIAL_FUNCTIONS_DETAIL_SINHC_H
 
 namespace special_functions::detail {
-    template<typename Tp>
-    emsr::fp_promote_t<Tp>
-    sinhc(Tp x)
-    {
-        if (std::isnan(x))
-            return emsr::quiet_NaN(x);
-        else if (std::abs(x) < Tp{4} * emsr::sqrt_min(x))
-            return Tp{1} + x * x / Tp{6};
-        else
-            return std::sinh(x) / x;
+    template<typename T>
+    special_functions::fp_promote_t<T>
+    sinhc(T x) {
+        if (std::isnan(x)) {
+            return std::numeric_limits<T>::quiet_NaN();
+        }
+
+        if (std::abs(x) < T{4} * special_functions::numeric_limits::sqrt_min(x)) {
+            return T{1} + x * x / T{6};
+        }
+
+        return std::sinh(x) / x;
     }
 }
 
--- include/special_functions/detail/sinhc_pi.h
+++ include/special_functions/detail/sinhc_pi.h
@@ -2,21 +2,18 @@
 #define SPECIAL_FUNCTIONS_DETAIL_SINHC_PI_H
 
 namespace special_functions::detail {
-    template<typename Tp>
-    emsr::fp_promote_t<Tp>
-    sinhc_pi(Tp x)
-    {
-        const auto s_pi = emsr::pi_v<Tp>;
-        if (std::isnan(x))
-            return emsr::quiet_NaN(x);
-        else
-        {
-            auto arg = s_pi * x;
-            if (std::abs(arg) < Tp{4} * emsr::sqrt_min(x))
-                return Tp{1} + arg * arg / Tp{6};
-            else
-                return emsr::sinh_pi(x) / arg;
+    template<typename T>
+    special_functions::fp_promote_t<T>
+    sinhc_pi(T x) {
+        if (std::isnan(x)) {
+            return std::numeric_limits<T>::quiet_NaN();
         }
+
+        if (std::abs(std::numbers::pi_v<T> * x) < T{4} * special_functions::numeric_limits::sqrt_min(x)) {
+            return T{1} + std::numbers::pi_v<T> * x * (std::numbers::pi_v<T> * x) / T{6};
+        }
+
+        return special_functions::sinh_pi(x) / (std::numbers::pi_v<T> * x);
     }
 }
 
