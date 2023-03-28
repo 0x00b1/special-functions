e4084e8c02bb4b60a1c373dbc7588795d29a3b8b
Allen Goodman <goodmaa3@gene.com>
2023-03-28 16:12:16 -0400

    smoke tests

--- include/special_functions/bell_polynomial_b.h
+++ include/special_functions/bell_polynomial_b.h
@@ -1,8 +1,14 @@
 #ifndef SPECIAL_FUNCTIONS_BELL_POLYNOMIAL_B_H
 #define SPECIAL_FUNCTIONS_BELL_POLYNOMIAL_B_H
 
-namespace special_functions {
+#include <special_functions/detail/bell_polynomial_b.h>
 
+namespace special_functions {
+    template<typename T, typename U>
+    inline U
+    bell_polynomial_b(unsigned int n, U x) {
+        // return special_functions::detail::bell_polynomial_b(n, x);
+    }
 }
 
 #endif // SPECIAL_FUNCTIONS_BELL_POLYNOMIAL_B_H
--- include/special_functions/lah_number.h
+++ include/special_functions/lah_number.h
@@ -2,7 +2,11 @@
 #define SPECIAL_FUNCTIONS_LAH_NUMBER_H
 
 namespace special_functions {
-
+    template<typename Tp>
+    inline Tp
+    lah_number(unsigned int n, unsigned int k) {
+        // return emsr::detail::lah<Tp>(n, k);
+    }
 }
 
 #endif // SPECIAL_FUNCTIONS_LAH_NUMBER_H
