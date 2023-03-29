55d96850c10e515f9989f847546eecc89e1ea001
Allen Goodman <goodmaa3@gene.com>
2023-03-29 14:41:48 -0400

    smoke test

--- include/special_functions/radial_polynomial_r.h
+++ include/special_functions/radial_polynomial_r.h
@@ -4,7 +4,13 @@
 #include <special_functions/detail/radial_polynomial_r.h>
 
 namespace special_functions {
+    template<typename T>
+    inline special_functions::fp_promote_t<T>
+    radial_polynomial_r(unsigned int n, unsigned int m, T r) {
+        using type = special_functions::fp_promote_t<T>;
 
+        // return emsr::detail::radial_jacobi<type>(n, m, r);
+    }
 }
 
 #endif // SPECIAL_FUNCTIONS_RADIAL_POLYNOMIAL_R_H
