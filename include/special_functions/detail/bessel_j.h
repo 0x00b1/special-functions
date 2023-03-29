adaec0f7022ea960457d0059e5d7b5321cba1e39
Allen Goodman <goodmaa3@gene.com>
2023-03-29 11:24:01 -0400

    smoke test

--- include/special_functions/detail/bessel_j.h
+++ include/special_functions/detail/bessel_j.h
@@ -1,6 +1,8 @@
 #ifndef SPECIAL_FUNCTIONS_DETAIL_BESSEL_J_H
 #define SPECIAL_FUNCTIONS_DETAIL_BESSEL_J_H
 
+#include "bessel_by_asymptotic_expansion.h"
+#include "bessel_by_series_expansion.h"
 #include "bessel_t.h"
 
 namespace special_functions::detail {}
