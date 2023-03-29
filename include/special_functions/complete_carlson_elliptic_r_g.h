bb685c0888d8830e0e3899f08e54e33426118990
Allen Goodman <goodmaa3@gene.com>
2023-03-29 12:32:45 -0400

    smoke test

--- include/special_functions/complete_carlson_elliptic_r_g.h
+++ include/special_functions/complete_carlson_elliptic_r_g.h
@@ -4,12 +4,12 @@
 #include <special_functions/detail/complete_carlson_elliptic_r_g.h>
 
 namespace special_functions {
-    template<typename _Tx, typename _Ty>
-    inline special_functions::fp_promote_t<_Tx, _Ty>
-    complete_carlson_elliptic_r_g(_Tx x, _Ty y)
-    {
-        using type = special_functions::fp_promote_t<_Tx, _Ty>;
-        return special_functions::detail::complete_carlson_elliptic_r_g<type>(x, y);
+    template<typename T, typename U>
+    inline special_functions::fp_promote_t<T, U>
+    complete_carlson_elliptic_r_g(T x, U y) {
+        using V = special_functions::fp_promote_t<T, U>;
+
+        return special_functions::detail::complete_carlson_elliptic_r_g<V>(x, y);
     }
 }
 
