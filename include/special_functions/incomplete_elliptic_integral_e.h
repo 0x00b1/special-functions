0f5f3943c349c296a6bb701523ff1e076d8ffd94
Allen Goodman <goodmaa3@gene.com>
2023-03-29 12:33:46 -0400

    smoke test

--- include/special_functions/incomplete_elliptic_integral_e.h
+++ include/special_functions/incomplete_elliptic_integral_e.h
@@ -6,10 +6,10 @@
 namespace special_functions {
     template<typename T, typename U>
     inline special_functions::fp_promote_t<T, U>
-    incomplete_elliptic_integral_e(T k, U phi) {
+    incomplete_elliptic_integral_e(T k, U p) {
         using V = special_functions::fp_promote_t<T, U>;
 
-        return special_functions::detail::incomplete_elliptic_integral_e<V>(k, phi);
+        return special_functions::detail::incomplete_elliptic_integral_e<V>(k, p);
     }
 }
 
