76cbccbcf9f911406b2c8d8f2f0e2a0235615b91
Allen Goodman <allen.goodman@icloud.com>
2025-09-05 15:28:47 -0400

    cleanup

--- build/CMakeFiles/fc-tmp/rapidcheck/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for rapidcheck")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/Users/goodmaa3/com/github/0x00b1/special-functions/build/CMakeFiles/fc-tmp/rapidcheck/rapidcheck-gitupdate.cmake")
-
-endblock()
--- build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/goodmaa3/com/github/0x00b1/special-functions/build/_deps/googletest-src/googlemock
-# Build directory: /Users/goodmaa3/com/github/0x00b1/special-functions/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
